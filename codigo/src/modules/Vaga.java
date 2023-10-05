c7755c64d446460315ead44e6d0fa03c006b1449
Luiz Felipe Saraiva Sério <1322173@sga.pucminas.br>
2023-10-04 22:18:10 -0300

    feature/classvaga metodo estacionar

--- codigo/src/modules/Vaga.java
+++ codigo/src/modules/Vaga.java
@@ -2,4 +2,25 @@ package modules;
 
 public class Vaga {
 
+	private String id;
+	private boolean disponivel;
+
+	public Vaga(int fila, int numero) {
+		
+	}
+
+	public boolean estacionar() {
+		disponivel = false;
+		return disponivel;
+		
+	}
+
+	public boolean sair() {
+		
+	}
+
+	public boolean disponivel() {
+		
+	}
+
 }

67cb61d10c69f769912fd76500c33a47d0d82552
Luís M <l.miguel-souza@hotmail.com>
2023-10-04 22:02:11 -0300

    imp: base da classe Veiculo

---
+++ codigo/src/modules/Veiculo.java
@@ -0,0 +1,30 @@
+package modules;
+
+public class Veiculo {
+    private String placa;
+    private UsoDeVaga usos = new UsoDeVaga[];
+
+    public Veiculo(String placa) {
+        this.placa = placa;
+    }
+
+    public void estacionar(Vaga vaga) {
+
+    }
+
+    public double sair() {
+
+    }
+
+    public double totalArrecadado() {
+
+    }
+
+    public double arrecadadoNoMes(int mes) {
+
+    }
+
+    public int totalDeUsos() {
+
+    }
+}

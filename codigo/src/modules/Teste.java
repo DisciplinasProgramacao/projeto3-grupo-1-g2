24498c65978191756bdffd410bd7e74213882028
Luiz Felipe Saraiva Serio <lfsserio@sga.pucminas.br>
2023-10-18 17:10:08 -0300

    Teste da classe vaga

--- codigo/src/modules/Teste.java
+++ codigo/src/modules/Teste.java
@@ -1,4 +1,39 @@
 package modules;
 
-public class Teste {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VagaTest {
+
+    private Vaga vaga;
+
+    @BeforeEach
+    public void setUp() {
+        vaga = new Vaga(1, 10);
+    }
+
+    @Test
+    public void testEstaDisponivelInicial() {
+        assertTrue(vaga.disponivel());
+    }
+
+    @Test
+    public void testEstacionarVaga() {
+        assertTrue(vaga.estacionar());
+        assertFalse(vaga.disponivel());
+    }
+
+    @Test
+    public void testEstacionarVagaRepetidamente() {
+        assertTrue(vaga.estacionar());
+        assertFalse(vaga.estacionar());
+    }
+
+    @Test
+    public void testSairVaga() {
+        assertTrue(vaga.estacionar());
+        assertTrue(vaga.sair());
+        assertTrue(vaga.disponivel());
+    }
 }

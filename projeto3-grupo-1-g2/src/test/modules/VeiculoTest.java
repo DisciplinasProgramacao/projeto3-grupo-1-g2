8f77fa60d0cec4e5da6231a5fee94f7ec90158e6
Luís M <l.miguel-souza@hotmail.com>
2023-11-08 19:26:23 -0300

    Correção classe de teste Veiculo

--- projeto3-grupo-1-g2/src/test/modules/VeiculoTest.java
+++ projeto3-grupo-1-g2/src/test/modules/VeiculoTest.java
@@ -1,45 +1,87 @@
 package test.modules;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class VeiculoTest {
+import java.time.LocalDateTime;
+
+import modules.Vaga;
+import modules.Veiculo;
+
+class VeiculoTest {
+
     @Test
-    void testArrecadadoNoMes() {
-
+    void testCriarVeiculoEObterPlaca() {
+        Veiculo veiculo = new Veiculo("ABC1234");
+        assertEquals("ABC1234", veiculo.getPlaca());
     }
 
     @Test
     void testEstacionar() {
+        Veiculo veiculo = new Veiculo("ABC1234");
+        Vaga vaga = new Vaga(1);
+        veiculo.estacionar(vaga);
 
-    }
-
-    @Test
-    void testGetPlaca() {
-
-    }
-
-    @Test
-    void testGetUsos() {
-
+        assertEquals(1, veiculo.getUsos().size());
+        assertEquals(vaga, veiculo.getUsos().get(0).getVaga());
     }
 
     @Test
     void testSair() {
+        Veiculo veiculo = new Veiculo("ABC1234");
+        Vaga vaga = new Vaga(1);
+        veiculo.estacionar(vaga);
+        veiculo.sair();
 
-    }
-
-    @Test
-    void testSetPlaca() {
-
+        assertNotNull(veiculo.getUsos().get(0).getSaida());
+        assertFalse(veiculo.getUsos().get(0).getValorPago() > 0);
     }
 
     @Test
     void testTotalArrecadado() {
+        Veiculo veiculo = new Veiculo("ABC1234");
+        Vaga vaga1 = new Vaga(1);
+        Vaga vaga2 = new Vaga(2);
 
+        veiculo.estacionar(vaga1);
+        veiculo.sair();
+
+        veiculo.estacionar(vaga2);
+        veiculo.sair();
+
+        double totalEsperado = veiculo.getUsos().get(0).getValorPago() + veiculo.getUsos().get(1).getValorPago();
+        assertEquals(totalEsperado, veiculo.totalArrecadado());
+    }
+
+    @Test
+    void testArrecadadoNoMes() {
+        Veiculo veiculo = new Veiculo("ABC1234");
+        Vaga vaga1 = new Vaga(1);
+        Vaga vaga2 = new Vaga(2);
+
+        veiculo.estacionar(vaga1);
+        veiculo.sair();
+
+        veiculo.estacionar(vaga2);
+        veiculo.sair();
+
+        int mesAtual = LocalDateTime.now().getMonthValue();
+        double totalEsperado = veiculo.getUsos().get(0).getValorPago() + veiculo.getUsos().get(1).getValorPago();
+        assertEquals(totalEsperado, veiculo.arrecadadoNoMes(mesAtual));
     }
 
     @Test
     void testTotalDeUsos() {
+        Veiculo veiculo = new Veiculo("ABC1234");
+        Vaga vaga1 = new Vaga(1);
+        Vaga vaga2 = new Vaga(2);
 
+        veiculo.estacionar(vaga1);
+        veiculo.sair();
+
+        veiculo.estacionar(vaga2);
+        veiculo.sair();
+
+        assertEquals(2, veiculo.totalDeUsos());
     }
 }

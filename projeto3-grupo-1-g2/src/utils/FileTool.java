dc4d0caed247ca3e5280ee026b35ccbca7893ca4
Luís M <L.miguel-souza@hotmail.com>
2023-11-25 23:08:04 -0300

    Correção FileTool para leitura de arquivo de dados

--- projeto3-grupo-1-g2/src/utils/FileTool.java
+++ projeto3-grupo-1-g2/src/utils/FileTool.java
@@ -2,6 +2,7 @@ package utils;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,22 +12,20 @@ public class FileTool {
     private FileWriter file_writer;
     private BufferedReader buffered_reader;
     private BufferedWriter buffered_writer;
+    private boolean append;
 
     /**
-	 * @param path
-	 *  Construtor do FileTool
-	 */
-    public FileTool(String path) throws IOException {
-        this.file_reader = new FileReader(path);
-        this.file_writer = new FileWriter(path);
-        this.buffered_reader = new BufferedReader(file_reader);
-        this.buffered_writer = new BufferedWriter(file_writer);
+     * @param path
+     *             Construtor do FileTool
+     */
+    public FileTool(boolean append) throws IOException {
+        this.append = append;
     }
 
     /**
-	 * @param index
-	 *  Retorna linha de leitura do arquivo
-	 */
+     * @param index
+     *              Retorna linha de leitura do arquivo
+     */
     public String readLine(int index) throws IOException {
         String line;
         int counter = 1;
@@ -46,23 +45,23 @@ public class FileTool {
     }
 
     /**
-	 * Retorna uma string com a leitura do arquivo por completo
-	 */
+     * Retorna uma string com a leitura do arquivo por completo
+     */
     public String readAll() throws IOException {
         String line;
         String text = "";
 
         while ((line = buffered_reader.readLine()) != null) {
-            text += line;
+            text += line + "\n";
         }
 
         return text;
     }
 
     /**
-	 * @param text
-	 *  Busca String dentro do arquivo procurado e retorna caso encontre
-	 */
+     * @param text
+     *             Busca String dentro do arquivo procurado e retorna caso encontre
+     */
     public String findText(String text) throws IOException {
         String line;
         String text_not_found = "Texto não encontrado.";
@@ -77,8 +76,8 @@ public class FileTool {
     }
 
     /**
-	 * Escreve no arquivo em uma linha nova.
-	 */
+     * Escreve no arquivo em uma linha nova.
+     */
     public boolean writeNewLine(String text) throws IOException {
         buffered_writer.newLine();
         buffered_writer.write(text);
@@ -87,9 +86,9 @@ public class FileTool {
     }
 
     /**
-	 * @param text
-	 *  Escreve no arquivo.
-	 */
+     * @param text
+     *             Escreve no arquivo.
+     */
     public boolean write(String text) throws IOException {
         buffered_writer.write(text);
 
@@ -97,14 +96,26 @@ public class FileTool {
     }
 
     /**
-	 *  Fecha as leituras dos arquivos.
-	 */
+     * Fecha as leituras dos arquivos.
+     */
     public void close() throws IOException {
         buffered_reader.close();
         buffered_writer.close();
+        file_reader.close();
+        file_writer.close();
     }
 
-    public void create(String path) {
-        
+    public boolean createFile(String path, String fileName) throws IOException {
+        File newFile = new File(path + fileName + ".txt");
+
+        boolean created = newFile.createNewFile();
+        return created;
+    }
+
+    public void changePath(String path) throws IOException {
+        this.file_reader = new FileReader(path);
+        this.file_writer = new FileWriter(path, this.append);
+        this.buffered_reader = new BufferedReader(file_reader);
+        this.buffered_writer = new BufferedWriter(file_writer);
     }
 }

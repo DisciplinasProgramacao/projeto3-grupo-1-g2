6dd473077ffe83d7bd6d4ce74df305da3dd07fb8
Luís M <L.miguel-souza@hotmail.com>
2023-11-25 23:37:30 -0300

    Colocando streams para processamento de listas

--- projeto3-grupo-1-g2/src/modules/Estacionamento.java
+++ projeto3-grupo-1-g2/src/modules/Estacionamento.java
@@ -40,22 +40,24 @@ public class Estacionamento {
             boolean possuiVeiculo = cliente.possuiVeiculo(veiculo.getPlaca());
 
             if (possuiVeiculo) {
-                for (Vaga vaga : vagas) {
-                    if (vaga.disponivel()) {
-                        veiculo.estacionar(vaga);
-                        return true;
-                    }
-                }
+                vagas.stream()
+                        .filter(Vaga::disponivel)
+                        .findFirst()
+                        .map(vaga -> {
+                            veiculo.estacionar(vaga);
+                            return true;
+                        })
+                        .orElse(false);
             }
         }
-
         return false;
     }
 
     private ArrayList<Vaga> gerarVagas(int numero) {
         for (int i = 0; i < this.quantFileiras; i++) {
             for (int j = 0; j < this.vagasPorFileira; j++) {
-                vagas.add(new Vaga(Integer.parseInt(String.valueOf(this.quantFileiras) + String.valueOf(this.vagasPorFileira))));
+                vagas.add(new Vaga(
+                        Integer.parseInt(String.valueOf(this.quantFileiras) + String.valueOf(this.vagasPorFileira))));
             }
         }
         return vagas;
@@ -116,10 +118,10 @@ public class Estacionamento {
 
     public boolean addClienteToEstacionamento(Cliente cliente) {
         List<Veiculo> veiculosCliente = cliente.getVeiculos();
-        
+
         if (!clientesVeiculos.containsKey(cliente)) {
             clientesVeiculos.put(cliente, veiculosCliente);
-            return true; 
+            return true;
         }
         return false;
     }

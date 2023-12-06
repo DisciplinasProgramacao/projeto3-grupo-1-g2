37dfc88d7b0fdbd5157cf4922bd07e0fe553087a
Seu Nome <rubensmarcelo31@gmail.com>
2023-12-06 00:11:45 -0300

    update: gerar relatório mensalista histórico de uso

--- projeto3-grupo-1-g2/src/modules/Mensalista.java
+++ projeto3-grupo-1-g2/src/modules/Mensalista.java
@@ -1,5 +1,8 @@
 package modules;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public class Mensalista extends Cliente{
 
 
@@ -15,6 +18,43 @@ public class Mensalista extends Cliente{
     @Override
     public double arrecadadoNoMes(int p_mes) {
         return 500.00;
+    }
+
+    public static void gerarRelatorioUsoMensalistas(List<Mensalista> mensalistas) {
+        int mesAtual = LocalDate.now().getMonthValue();
+        int anoAtual = LocalDate.now().getYear();
+        int totalUsosMensalistas = 0;
+
+        StringBuilder relatorio = new StringBuilder();
+        relatorio.append("Relatório de Uso dos Clientes Mensalistas\n");
+        relatorio.append("Mês/Ano Atual: ").append(mesAtual).append("/").append(anoAtual).append("\n");
+
+        for (Mensalista mensalista : mensalistas) {
+            int totalUsosMensalista = mensalista.getVeiculos().stream()
+                .mapToInt(veiculo -> veiculo.getUsos().size())
+                .sum();
+            totalUsosMensalistas += totalUsosMensalista;
+        }
+
+        for (Mensalista mensalista : mensalistas) {
+            int usosMensalista = mensalista.getVeiculos().stream()
+                .mapToInt(veiculo -> (int) veiculo.getUsos().stream()
+                    .filter(uso -> uso.getEntrada().getMonthValue() == mesAtual &&
+                                   uso.getEntrada().getYear() == anoAtual)
+                    .count())
+                .sum();
+
+            double percentualUso = totalUsosMensalistas > 0 ? 
+                                   (double) usosMensalista / totalUsosMensalistas * 100 : 0;
+
+            relatorio.append("Cliente: ").append(mensalista.getNome())
+                    .append(", CPF: ").append(mensalista.getCpf())
+                    .append(", Total de Usos no Mês: ").append(usosMensalista)
+                    .append(", Percentual de Uso: ").append(String.format("%.2f", percentualUso)).append("%")
+                    .append("\n");
+        }
+
+        System.out.println(relatorio.toString());
     }
 
 }

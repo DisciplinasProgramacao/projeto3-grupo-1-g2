6984a4eead3a3a7d40a52690daeefc1a5ead195e
erica-as <erica.asnt@hotmail.com>
2023-12-06 18:52:49 -0300

    documentação javadoc classe mensalista

--- projeto3-grupo-1-g2/src/modules/Mensalista.java
+++ projeto3-grupo-1-g2/src/modules/Mensalista.java
@@ -3,23 +3,50 @@ package modules;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * Representa um cliente do tipo Mensalista, que é uma subclasse de Cliente.
+ * Clientes Mensalistas têm métodos específicos para cálculos de arrecadação e relatórios de uso.
+ *
+ */
 public class Mensalista extends Cliente{
+    //#region Construtores
 
-
+    /**
+     * Construtor da classe Mensalista.
+     *
+     * @param p_nome O nome do cliente Mensalista.
+     * @param p_cpf O CPF do cliente Mensalista.
+     */
     public Mensalista(String p_nome, String p_cpf) {
         super(p_nome, p_cpf);
     }
 
+    /**
+     * Retorna o valor total arrecadado pelos veículos do cliente Mensalista.
+     *
+     * @return O valor total arrecadado pelos veículos do cliente Mensalista.
+     */
     @Override
     public double arrecadadoTotal() {
         return 500.00;
     }
 
+    /**
+     * Retorna o valor arrecadado pelos veículos do cliente Mensalista no mês especificado.
+     *
+     * @param p_mes O mês para o qual se deseja calcular a arrecadação.
+     * @return O valor arrecadado pelos veículos do cliente Mensalista no mês.
+     */
     @Override
     public double arrecadadoNoMes(int p_mes) {
         return 500.00;
     }
 
+    /**
+     * Gera um relatório de uso dos clientes Mensalistas para o mês/ano atual.
+     *
+     * @param mensalistas A lista de clientes Mensalistas para os quais o relatório será gerado.
+     */
     public static void gerarRelatorioUsoMensalistas(List<Mensalista> mensalistas) {
         int mesAtual = LocalDate.now().getMonthValue();
         int anoAtual = LocalDate.now().getYear();

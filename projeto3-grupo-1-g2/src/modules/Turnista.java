1d45a79acb395bfce10de18b518d73e96d6d13fc
erica-as <erica.asnt@hotmail.com>
2023-12-06 18:55:17 -0300

    documentação javadoc classe turnista

--- projeto3-grupo-1-g2/src/modules/Turnista.java
+++ projeto3-grupo-1-g2/src/modules/Turnista.java
@@ -3,15 +3,34 @@ package modules;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
+/**
+ * Representa um cliente do tipo Turnista, que é uma subclasse de Cliente.
+ * Clientes Turnistas têm métodos específicos para cálculos de arrecadação e controle de turno.
+ *
+ */
 public class Turnista extends Cliente{
+    //#region atributos
     private String turno;
-
     private double pagoParte;
+    //#endregion
 
+    //#region Construtores
+
+    /**
+     * Construtor da classe Turnista.
+     *
+     * @param p_nome O nome do cliente Turnista.
+     * @param p_cpf O CPF do cliente Turnista.
+     */
     public Turnista(String p_nome, String p_cpf) {
         super(p_nome, p_cpf);
     }
 
+    /**
+     * Obtém o período do dia atual com base na hora do sistema.
+     *
+     * @return O período do dia atual (manha, tarde, noite).
+     */
     public static String obterPeriodoDoDia() {
         LocalDateTime dateTime = LocalDateTime.now();
         int hora = dateTime.getHour();
@@ -24,6 +43,12 @@ public class Turnista extends Cliente{
         }
     }
 
+    /**
+     * Verifica se o turno do cliente Turnista coincide com o período do dia atual.
+     * Se não coincidir, registra a entrada do cliente no estacionamento.
+     *
+     * @return TRUE se o turno coincide, FALSE se o cliente entra em um turno diferente.
+     */
     public boolean verificaTurno(){
         String periodo = obterPeriodoDoDia();
         if(turno.equals(periodo)){
@@ -34,16 +59,33 @@ public class Turnista extends Cliente{
         }
     }
 
+    /**
+     * Calcula o valor total arrecadado pelos veículos do cliente Turnista.
+     *
+     * @return O valor total arrecadado pelos veículos do cliente Turnista.
+     */
     @Override
     public double arrecadadoTotal() {
        return 200 + pagoParte;
     }
 
+    /**
+     * Calcula o valor arrecadado pelos veículos do cliente Turnista no mês especificado.
+     *
+     * @param p_mes O mês para o qual se deseja calcular a arrecadação.
+     * @return O valor arrecadado pelos veículos do cliente Turnista no mês.
+     */
     @Override
     public double arrecadadoNoMes(int p_mes) {
         return 0;
     }
 
+    /**
+     * Registra a entrada do cliente Turnista no estacionamento e calcula o valor a ser pago.
+     *
+     * @param entrada A data e hora de entrada no estacionamento.
+     * @return O valor a ser pago pelo cliente Turnista.
+     */
     public double entradaEstacionamento(LocalDateTime entrada){
         LocalDateTime saida = LocalDateTime.now().plus(30, ChronoUnit.MINUTES);
         boolean isTurno = verificaTurno();

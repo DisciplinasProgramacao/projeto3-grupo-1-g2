355365c950cb3efd1620918a307bb09054dc094f
Seu Nome <rubensmarcelo31@gmail.com>
2023-12-06 00:05:20 -0300

    update: gerar relatório horista arrecadação mensal

--- projeto3-grupo-1-g2/src/modules/Horista.java
+++ projeto3-grupo-1-g2/src/modules/Horista.java
@@ -1,5 +1,7 @@
 package modules;
 
+import java.util.List;
+
 public class Horista extends Cliente {
 
     public Horista(String p_nome, String p_cpf) {
@@ -21,6 +23,29 @@ public class Horista extends Cliente {
             totalMes += veiculo.arrecadadoNoMes(p_mes);
         }
         return totalMes;
+    }
+
+    public static void gerarRelatorioArrecadacaoMedia(List<Horista> horistas, int mes) {
+        double totalArrecadado = 0;
+        int totalClientes = horistas.size();
+
+        StringBuilder relatorio = new StringBuilder();
+        relatorio.append("Relatório de Arrecadação Média dos Clientes Horistas\n");
+        relatorio.append("Mês Especificado: ").append(mes).append("\n");
+
+        for (Horista horista : horistas) {
+            double arrecadadoNoMes = horista.arrecadadoNoMes(mes);
+            totalArrecadado += arrecadadoNoMes;
+            relatorio.append("Cliente: ").append(horista.getNome())
+                    .append(", CPF: ").append(horista.getCpf())
+                    .append(", Arrecadado no Mês: R$").append(arrecadadoNoMes)
+                    .append("\n");
+        }
+
+        double mediaArrecadacao = totalClientes > 0 ? totalArrecadado / totalClientes : 0;
+        relatorio.append("Arrecadação Média no Mês Especificado: R$").append(mediaArrecadacao).append("\n");
+
+        System.out.println(relatorio.toString());
     }
 
 }

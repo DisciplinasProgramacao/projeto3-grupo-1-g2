d4d4558d7b160411b760e7ca8fd806451fc4dadd
erica-as <erica.asnt@hotmail.com>
2023-12-06 19:06:07 -0300

    documentação javadoc classe veiculo

--- projeto3-grupo-1-g2/src/modules/Veiculo.java
+++ projeto3-grupo-1-g2/src/modules/Veiculo.java
@@ -8,33 +8,55 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Veiculo {
-
+    //#region atributos
     private String placa;
     private List<UsoDeVaga> usos;
+    //#endregion
 
+    //#region Construtores
+
+    /**
+     * Construtor da classe Veiculo.
+     *
+     * @param placa A placa única associada ao veículo.
+     */
     public Veiculo(String placa) {
         this.placa = placa;
         this.usos = new ArrayList<UsoDeVaga>();
     }
 
-    public String getPlaca() {
-        return placa;
-    }
-
-    public List<UsoDeVaga> getUsos() {
-        return usos;
-    }
-
+    /**
+     * Estaciona o veículo em uma vaga, registrando o uso.
+     *
+     * @param vaga A vaga onde o veículo será estacionado.
+     * @param usadoManobrista Indica se foi utilizado serviço de manobrista.
+     * @param usadoLavagem Indica se foi utilizado serviço de lavagem.
+     * @param usadoPolimento Indica se foi utilizado serviço de polimento.
+     */
     public void estacionar(Vaga vaga,  boolean usadoManobrista, boolean usadoLavagem, boolean usadoPolimento) {
         UsoDeVaga uso = new UsoDeVaga(vaga, LocalDateTime.now(), null, usadoManobrista, usadoLavagem, usadoPolimento);
         usos.add(uso);
         vaga.estacionar();
     }
 
+    /**
+     * Registra a saída do veículo da vaga com base no tempo de permanência especificado.
+     *
+     * @param p_horario O tempo de permanência em minutos.
+     * @return O valor a ser pago pelo uso da vaga.
+     * @throws Exception Exceção lançada em caso de problemas no processo de saída.
+     */
     public double sair(Integer p_horario) throws Exception {
         return sairComParametro(p_horario);
     }
 
+    /**
+     * Método privado para registrar a saída do veículo com base no tempo de permanência especificado.
+     *
+     * @param p_horario O tempo de permanência em minutos.
+     * @return O valor a ser pago pelo uso da vaga.
+     * @throws Exception Exceção lançada em caso de problemas no processo de saída.
+     */
     private double sairComParametro(Integer p_horario) throws Exception {
         UsoDeVaga ultimoUso = usos.get(usos.size() - 1);
         try {
@@ -45,6 +67,11 @@ public class Veiculo {
         }
     }
 
+    /**
+     * Calcula o total arrecadado pelos usos de vaga associados a este veículo.
+     *
+     * @return O total arrecadado.
+     */
     public double totalArrecadado() {
         double total = 0;
         for (UsoDeVaga uso : usos) {
@@ -53,6 +80,12 @@ public class Veiculo {
         return total;
     }
 
+    /**
+     * Calcula o total arrecadado pelos usos de vaga associados a este veículo em um determinado mês.
+     *
+     * @param mes O número do mês.
+     * @return O total arrecadado no mês especificado.
+     */
     public double arrecadadoNoMes(int mes) {
         double totalMes = 0;
         for (UsoDeVaga uso : usos) {
@@ -63,6 +96,11 @@ public class Veiculo {
         return totalMes;
     }
 
+    /**
+     * Gera um relatório formatado com informações sobre os usos de vaga associados a este veículo.
+     *
+     * @return O relatório formatado.
+     */
     public String gerarRelatorioVagas() {
         DateTimeFormatter dfm = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm");
 
@@ -87,7 +125,20 @@ public class Veiculo {
         return sb.toString();
     }
 
+    /**
+     * Obtém o número total de usos de vaga associados a este veículo.
+     *
+     * @return O número total de usos.
+     */
     public int totalDeUsos() {
         return usos.size();
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public List<UsoDeVaga> getUsos() {
+        return usos;
     }
 }
